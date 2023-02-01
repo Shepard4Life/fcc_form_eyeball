a0de229429514319cc22abd6bcaf35cc2f8ae607
Kamil <kamil.zmurek@gmail.com>
2023-02-01 19:58:45 +0100

    Background image correction

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function aliasGen(one, two){
+    if (/^[a-zA-Z]+$/.test(one), /^[a-zA-Z]+$/.test(two)){
+      var first = one[0];
+      var last =  two[0];
+      return (firstName.first, surname.last);
+    }
+    else {
+      return 'Your name must start with a letter from A - Z.';
+      }
+}
